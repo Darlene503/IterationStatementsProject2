51fa70a7c932b4e0021a20dbb72f8f8aba77e29d
darlene503 <duyn503@gmail.com>
2023-07-27 22:46:31 -0700

    equalornot

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -13,6 +13,8 @@
 
             PrintNumbers();
             ThreebyThree();
+            Console.WriteLine(EqualorNot(3, 5));
+
         }
         public static void PrintNumbers()
         {
@@ -23,40 +25,77 @@
             }
 
 
-          //Write a method that will print to the console numbers 3 through 999 by 3 each time
+            //Write a method that will print to the console numbers 3 through 999 by 3 each time
 
         }
         public static void ThreebyThree()
         {
-           
-                for (int z = 3; z <= 999; z += 3)
-                {
+
+            for (int z = 3; z <= 999; z += 3)
+            {
                 Console.WriteLine(z);
-                }
+            }
+
+
+
+            //Write a method to accept two integers as parameterss and check whether they are equal or not
         }
-           
+        public static bool EqualorNot(int e, int d)
+        {
+            if (e == d)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
     }
 
 }
 
 
 
-        //Write a method to accept two integers as parameterss and check whether they are equal or not
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         //Write a method to check whether a given number is even or odd
-        
-        //Write a method to check whether a given number is positive or negative
-        
-        //Write a method to read the age of a candidate and determine whether they can vote.
-        //Hint: Use Parse or the safer TryParse() for an extra challenge
-        //Parse()
-        //TryParse()
 
-        //Heatin Up Section:
-        //Write a method to check if an integer(from the user) is in the range -10 to 10
-        
-        //Write a method to display the multiplication table(from 1 to 12) of a given integer
+//Write a method to check whether a given number is positive or negative
+
+//Write a method to read the age of a candidate and determine whether they can vote.
+//Hint: Use Parse or the safer TryParse() for an extra challenge
+//Parse()
+//TryParse()
+
+//Heatin Up Section:
+//Write a method to check if an integer(from the user) is in the range -10 to 10
+
+//Write a method to display the multiplication table(from 1 to 12) of a given integer
 
 
-        //Call the methods to test them in the Main method below
-        
+//Call the methods to test them in the Main method below
+
